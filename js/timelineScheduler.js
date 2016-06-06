7f332b90e2bbc7f911b500b4fbfa7b0938e5bf2b
olivelv <adalxh@gmail.com>
2016-06-06 16:55:36 +0800

    fix hover/click/leave function on event

--- js/timelineScheduler.js
+++ js/timelineScheduler.js
@@ -554,7 +554,8 @@ var TimeScheduler = {
                 if (!inSection[item.sectionID]) {
                     inSection[item.sectionID] = [];
                 }
-
+				//console.log(item);
+				//console.log("createItems: end"+end+",start:"+TimeScheduler.Options.Start);
                 if (item.start <= end && item.end >= TimeScheduler.Options.Start) {
                     foundPos = null;
 
@@ -598,19 +599,22 @@ var TimeScheduler = {
                                 .css('left', eventDiff + '%')
                                 .attr('title', event.at.format(TimeScheduler.Options.LowerFormat) + ' - ' + event.label)
                                 .data('event', event)
-                                .appendTo(itemElem);
+								.appendTo(itemContent);
+                                //.appendTo(itemElem);
                         }
                     }
 
                     if (item.start >= TimeScheduler.Options.Start) {
                         $(document.createElement('div'))
                             .addClass('time-sch-item-start')
-                            .appendTo(itemElem);
+							.appendTo(itemContent);
+                            //.appendTo(itemElem);
                     }
                     if (item.end <= end) {
                         $(document.createElement('div'))
                             .addClass('time-sch-item-end')
-                            .appendTo(itemElem);
+							.appendTo(itemContent);
+                            //.appendTo(itemElem);
                     }
 
                     item.Element = itemElem;
